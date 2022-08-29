3db83eba0a043db43f4379482a98017190cc3b1a
Vanshaj Vohra <vanshajvohra@gmail.com>
2022-08-29 23:19:25 +0530

    budget row

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
 
 const App = () => {
     return (
         <div className='container'>
             <h1 className='mt-3'>Budget Tracker</h1>
-            <p>pushin p</p>
+            <div className='row mt-3'>
+                <div className='col-sm'>
+                    <Budget />
+                </div>
+            </div> 
         </div>
     )
 }
