5f91d2f3fabc9ca286e3e1c85b1a30dcc78be408
Vanshaj Vohra <vanshajvohra@gmail.com>
2022-08-29 22:47:17 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+    return (
+        <div className='container'>
+            <h1 className='mt-3'>Budget Tracker</h1>
+        </div>
+    )
+}
+
+export default App;
