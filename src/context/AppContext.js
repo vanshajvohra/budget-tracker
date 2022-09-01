982073495821455f24e998410fa328d09ec0bf51
Vanshaj Vohra <vanshajvohra@gmail.com>
2022-09-01 17:12:54 +0530

    deleting an expense

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -8,6 +8,18 @@ const AppReducer = (state, action) => {
                 ...state,
                 expenses: [...state.expenses, action.payload]
             }
+        case 'DELETE_EXPENSE':
+            return {
+                ...state,
+                expenses: state.expenses.filter(
+                    (expense) => expense.id !== action.payload
+                ),
+            };
+        case 'SET_BUDGET':
+            return {
+                ...state,
+                budget: action.payload,
+            };
         default: return state;
     }
 };
