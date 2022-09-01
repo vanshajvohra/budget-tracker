3ae0a90143421a7776c040cf77dd574bdf8d136e
Vanshaj Vohra <vanshajvohra@gmail.com>
2022-09-01 15:16:18 +0530

    remaining & spent functionality

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const App = () => {
+    const { expenses } = useContext(AppContext);
+    const total = expenses.reduce((total, item) => {
+		return (total = total + item.cost);
+    }, 0);
+
+    const alertTypetwo = total > 1000 ? 'alert-warning' : 'alert-success';
+
     return (
-        <div className="alert alert-success">
-            <span>Total Spent: $750</span>
+        <div className={`alert p-3 ${alertTypetwo}`}>
+            <span>Total Spent: ${total}</span>
         </div>
     )
 }
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const App = () => {
+    const { expenses, budget } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item)=>{
+        return (total = total + item.cost);
+    }, 0);
+
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-secondary';
+
     return (
-        <div className="alert alert-danger">
-            <span>Remaining: $500</span>
+        <div className={`alert p-3 ${alertType}`}>
+            <span>Remaining: ${budget - totalExpenses}</span>
         </div>
     )
 }
